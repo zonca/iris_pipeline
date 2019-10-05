b567e4036d19fcd815b5cce1f103700d804a27bf
Andrea Zonca <code@andreazonca.com>
2019-10-04 20:33:24 -0700

    set ini file

--- setup_local_crds.sh
+++ setup_local_crds.sh
@@ -1,3 +1,5 @@
 export CRDS_PATH="$HOME/crds_cache"
 export CRDS_CONTEXT="tmt_0001.pmap"
 export CRDS_SERVER_URL="https://crds-serverless-mode.stsci.edu"
+export CRDS_INI_FILE=$CRDS_PATH/crds.ini
+export CRDS_ALLOW_BAD_PARKEY_VALUES=1
