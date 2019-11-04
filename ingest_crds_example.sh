0dce4f7ad978e4592b61ab7b5c5e0f93a2373a9e
Andrea Zonca <code@andreazonca.com>
2019-11-04 15:04:56 -0800

    example ingest flat into CRDS

---
+++ ingest_crds_example.sh
@@ -0,0 +1,6 @@
+crds refactor2 insert_reference --verbose --old-rmap \
+    ~/crds_cache/mappings/tmt/tmt_iris_flat_0003.rmap --new-rmap \
+    ~/crds_cache/mappings/tmt/tmt_iris_flat_0004.rmap \
+    --instruments IRIS \
+    --references iris_pipeline/tests/data/reference_flat2.fits
+#    --best-effort \
