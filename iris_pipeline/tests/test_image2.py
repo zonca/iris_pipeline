30e457cc73fee123afefdafe07a1432ec02a939b
Andrea Zonca <code@andreazonca.com>
2020-01-20 16:15:26 -0800

    test: unit test for image2 with subarray
    but it fails due to the 1-off error in flat field and
    the science input, fix once that 1-off error is fixed

--- iris_pipeline/tests/test_image2.py
+++ iris_pipeline/tests/test_image2.py
@@ -27,5 +27,40 @@ def test_image2():
          iris_pipeline.datamodels.IRISImageModel(ref_filename) as ref:
         np.testing.assert_allclose(out.data, ref.data, rtol=1e-6)
 
+def test_image2_subarray(tmp_path):
+    with open("data/asn_subtract_bg_flat.json") as fp:
+        asn = load_asn(fp)
+    raw_science_filename = get_data_from_url("17903858")
+    input_model = iris_pipeline.datamodels.IRISImageModel(raw_science_filename)
+
+    xstart = 100
+    ystart = 200
+    xsize = 50
+    ysize = 60
+    input_model.meta.subarray.name = "CUSTOM"
+    input_model.meta.subarray.xstart = xstart
+    input_model.meta.subarray.ystart = ystart
+    input_model.meta.subarray.xsize = xsize
+    input_model.meta.subarray.ysize = ysize
+    subarray_slice = np.s_[ystart:ystart+ysize, xstart:xstart+xsize]
+
+    input_model.data = input_model.data[subarray_slice]
+    input_model.dq = input_model.dq[subarray_slice]
+    input_model.err = input_model.err[subarray_slice]
+
+    raw_science_subarray_filename = tmp_path / "temp_subarray_science.fits"
+    input_model.write(raw_science_subarray_filename)
+
+    raw_background_filename = get_data_from_url("17903855")
+    asn["products"][0]["members"][0]["expname"] = str(raw_science_subarray_filename)
+    asn["products"][0]["members"][1]["expname"] = raw_background_filename
+    with open("test_asn.json", "w") as out_asn:
+        json.dump(asn, out_asn)
+    iris_pipeline.pipeline.Image2Pipeline.call("test_asn.json", config_file="data/image2_iris.cfg")
+    ref_filename = get_data_from_url("17905553")
+    with iris_pipeline.datamodels.IRISImageModel("test_iris_subtract_bg_flat_cal.fits") as out, \
+         iris_pipeline.datamodels.IRISImageModel(ref_filename) as ref:
+        np.testing.assert_allclose(out.data, ref.data[subarray_slice], rtol=1e-6)
+
 if __name__ == "__main__":
     test_image2()
