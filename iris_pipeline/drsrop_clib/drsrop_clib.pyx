ded7661f3691a933997c00dbbece8da97a7433eb
arunsurya77 <arunsuryaoffice@gmail.com>
2020-02-18 13:49:18 -0800

    Adding Submodule

--- iris_pipeline/drsrop_clib/drsrop_clib.pyx
+++ iris_pipeline/drsrop_clib/drsrop_clib.pyx
@@ -1,4 +1,4 @@
-cdef extern from "../iris_readout/drsrop.c":
+cdef extern from "../iris_readout/src/drsrop.c":
     float* uptheramp (int* arr, int* time, int a, int b, int c)
     float* mcds (int* arr, int* time, int a, int b, int c, int num_coadd)
     float* nonlin_corr (int* arr, int* time, int a, int b, int c, int x0, int y0, float* c0, float* c1, float* c2, float* c3, float* c4)
