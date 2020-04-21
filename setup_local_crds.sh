f86a4f9dab78f4084acddfb5a76a4c8e95c9eaf7
Andrea Zonca <code@andreazonca.com>
2020-04-21 14:02:50 -0700

    Print warning in setup script
    See https://github.com/oirlab/iris_pipeline/issues/20

--- setup_local_crds.sh
+++ setup_local_crds.sh
@@ -1,3 +1,6 @@
+echo "Make sure you run this script with
+echo "> source setup_local_crds.sh"
+echo "So that the variables are setup in the current session"
 export CRDS_PATH="$HOME/crds_cache"
 export CRDS_CONTEXT="tmt_0001.pmap"
 export CRDS_SERVER_URL="https://crds-serverless-mode.stsci.edu"
