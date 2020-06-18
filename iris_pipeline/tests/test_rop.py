691235dc5894a51649ea43ac8f74d157fa15089d
Andrea Zonca <code@andreazonca.com>
2020-06-18 13:58:08 -0700

    fix: use paths from base of repo

--- iris_pipeline/tests/test_rop.py
+++ iris_pipeline/tests/test_rop.py
@@ -3,7 +3,4 @@ import iris_pipeline
 iris_pipeline.monkeypatch_jwst_datamodels()
 
 def test_rop1():
-    iris_pipeline.pipeline.ROPPipeline.call('data/test_ramp.fits', config_file="data/drsrop.cfg")
-    return 1
-
-
+    iris_pipeline.pipeline.ROPPipeline.call('iris_pipeline/tests/data/test_ramp.fits', config_file="iris_pipeline/tests/data/drsrop.cfg")
