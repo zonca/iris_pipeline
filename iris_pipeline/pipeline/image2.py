04cf125498415b4b3105e3a093d8a904c0de01ec
Andrea Zonca <code@andreazonca.com>
2020-08-31 23:45:32 -0700

    fix: handle singleton

--- iris_pipeline/pipeline/image2.py
+++ iris_pipeline/pipeline/image2.py
@@ -60,6 +60,11 @@ class ProcessImagerL2Pipeline(Pipeline):
             self.log.info("Processing product {}".format(product["name"]))
             if self.save_results:
                 self.output_file = product["name"]
+            try:
+                getattr(asn, 'filename')
+            except AttributeError:
+                asn.filename = "singleton"
+
             result = self.process_exposure_product(
                 product, asn["asn_pool"], op.basename(asn.filename)
             )
