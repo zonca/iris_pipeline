10fe06aa399b82d7b73dd1798790c6bb63d8e70a
Andrea Zonca <code@andreazonca.com>
2020-10-08 16:53:40 -0700

    setup nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -203,36 +203,45 @@ intersphinx_mapping = {"jwst": ("https://jwst-pipeline.readthedocs.io/en/latest"
 extensions += ["sphinx_multiversion"]
 
 templates_path = [
-            "_templates",
-            ]
+    "_templates",
+]
 
-#html_sidebars = {'**':  [
+# html_sidebars = {'**':  [
 #            "versioning.html",
 #            ]}
 
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'^.*$'
+smv_tag_whitelist = r"^.*$"
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r'^master$'
+smv_branch_whitelist = r"^master$"
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = None
 
 # Pattern for released versions
-smv_released_pattern = r'^.*tags.*$'
+smv_released_pattern = r"^.*tags.*$"
 
 # Format for versioned output directories inside the build directory
-smv_outputdir_format = '{ref.name}'
+smv_outputdir_format = "{ref.name}"
 
 # Determines whether remote or local git branches/tags are preferred if their output dirs conflict
 smv_prefer_remote_refs = False
 
 latex_elements = {
     # Additional stuff for the LaTeX preamble.
-    'preamble': "".join((
-        '\DeclareUnicodeCharacter{00A0}{ }',  # NO-BREAK SPACE
-        '\DeclareUnicodeCharacter{251C}{+}',  # BOX DRAWINGS LIGHT VERTICAL AND RIGHT
-        '\DeclareUnicodeCharacter{2514}{+}',  # BOX DRAWINGS LIGHT UP AND RIGHT
-    )),
+    "preamble": "".join(
+        (
+            "\DeclareUnicodeCharacter{00A0}{ }",  # NO-BREAK SPACE
+            "\DeclareUnicodeCharacter{251C}{+}",  # BOX DRAWINGS LIGHT VERTICAL AND RIGHT
+            "\DeclareUnicodeCharacter{2514}{+}",  # BOX DRAWINGS LIGHT UP AND RIGHT
+        )
+    ),
 }
+
+extensions += [
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+]
+nbsphinx_kernel_name = "python3"
+nbsphinx_timeout = 600
