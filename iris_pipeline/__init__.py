adcd80a8ed69ebc3cb1444690b9f0b2dc6cc67f8
Andrea Zonca <code@andreazonca.com>
2020-10-08 21:26:47 -0700

    single-source version

--- iris_pipeline/__init__.py
+++ iris_pipeline/__init__.py
@@ -1,16 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-
 # ----------------------------------------------------------------------------
 
 # Enforce Python version check during package import.
 # This is the same check as the one at the top of setup.py
 import sys
 
+__version__ = "0.4.dev"
 __minimum_python_version__ = "3.6"
 
 
@@ -24,10 +20,6 @@ if sys.version_info < tuple(
     raise UnsupportedPythonError(
         "iris_pipeline does not support Python < {}".format(__minimum_python_version__)
     )
-
-if not _ASTROPY_SETUP_:
-    # For egg_info test builds to pass, put package imports here.
-    pass
 
 from .flatfield import *
 from .background import *
