21280996cbec9e7ce68ddb9d7e1f40899f9772ca
Andrea Zonca <code@andreazonca.com>
2020-10-08 22:14:21 -0700

    change name to fix issue in PDF

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg["name"]
+project = "IRISpipeline"
 author = setup_cfg["author"]
 copyright = "{0}, {1}".format(datetime.datetime.now().year, setup_cfg["author"])
 
