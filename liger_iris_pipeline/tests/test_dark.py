d65f30bd491511825be8c39af2341c56288f17e4
Andrea Zonca <code@andreazonca.com>
2024-06-16 23:49:41 -0700

    test: reworked test for pytest

--- liger_iris_pipeline/tests/test_dark.py
+++ liger_iris_pipeline/tests/test_dark.py
@@ -2,29 +2,33 @@
 import liger_iris_pipeline
 liger_iris_pipeline.monkeypatch_jwst_datamodels()
 import numpy as np
-np.random.seed(100)
-from test_utils import get_data_from_url
+from liger_iris_pipeline.tests.test_utils import get_data_from_url
 from jwst import datamodels
 from liger_iris_pipeline.dark_current.dark_sub import do_correction
 
-# Grab simulated raw frame
-raw_science_filename = get_data_from_url("17903858")
-input_model = datamodels.open(raw_science_filename)
+def test_dark_subtraction():
+    # Grab simulated raw frame
+    raw_science_filename = get_data_from_url("47090569")
+    input_model = datamodels.open(raw_science_filename)
 
-# Create a dark image with randomized vals centered around zero
-dark_model = liger_iris_pipeline.datamodels.DarkModel(data=np.random.normal(size=(4096, 4096)))
+    # Create a dark image with randomized vals centered around zero
+    dark_model = liger_iris_pipeline.datamodels.DarkModel(data=np.random.normal(size=(4096, 4096)))
 
-# Manual subtraction
-expected_output_data = input_model.data - dark_model.data
+    # Manual subtraction
+    expected_output_data = input_model.data - dark_model.data
 
-# Test do_correction method
-output = do_correction(input_model, dark_model)
-np.testing.assert_allclose(output.data, expected_output_data)
+    # Test do_correction method
+    output = do_correction(input_model, dark_model)
+    np.testing.assert_allclose(output.data, expected_output_data)
 
-# Test DarkCurrentStep class
-step = liger_iris_pipeline.dark_current.DarkCurrentStep(output_ext=".fits")
-step_output = step.run(raw_science_filename)
-step_dark_model = datamodels.open(step.dark_name)
+def test_dark_step():
+    raw_science_filename = get_data_from_url("47090569")
+    input_model = datamodels.open(raw_science_filename)
 
-# Test
-np.testing.assert_allclose(step_output.data, input_model.data - step_dark_model.data)
+    # Test DarkCurrentStep class
+    step = liger_iris_pipeline.dark_current.DarkCurrentStep()
+    step_output = step.run(raw_science_filename)
+    step_dark_model = datamodels.open(step.dark_name)
+
+    # Test
+    np.testing.assert_allclose(step_output.data, input_model.data - step_dark_model.data)
