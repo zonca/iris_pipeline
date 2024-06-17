701a431a0ed01e138776ab4098ed29cb513c353f
Andrea Zonca <code@andreazonca.com>
2024-06-16 23:26:24 -0700

    FIXME: override datamodels.open

--- liger_iris_pipeline/dark_current/dark_current_step.py
+++ liger_iris_pipeline/dark_current/dark_current_step.py
@@ -22,6 +22,19 @@ class DarkCurrentStep(Step):
 
     reference_file_types = ["dark"]
 
+    #FIXME - This will need to be ported to a new LigerIrisStep class
+    # I do not understand why this is necessary, in JWST it seems like
+    # this is not needed and works out of the box.
+    # Without this, the pipeline tries to call `datamodes.open` on a
+    # file that is already open, which gives the error:
+    # expected str, bytes or os.PathLike object, not LigerIrisDataModel
+    @classmethod
+    def _datamodels_open(cls, init, **kwargs):
+        if issubclass(init.__class__, stdatamodels.model_base.DataModel):
+            return init
+        else:
+            return datamodels.open(init, **kwargs)
+
     def process(self, input):
 
         # Open the input data model
